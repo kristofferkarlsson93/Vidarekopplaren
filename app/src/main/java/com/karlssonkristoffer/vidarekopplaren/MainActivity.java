66ca43cf2464cd4a3bff8d7b700690a735830c62
kristofferkarlsson93 <kristoffer.karlsson93@hotmail.com>
2017-10-13 22:44:52 +0200

    A bit new layout.

--- app/src/main/java/com/karlssonkristoffer/vidarekopplaren/MainActivity.java
+++ app/src/main/java/com/karlssonkristoffer/vidarekopplaren/MainActivity.java
@@ -10,17 +10,22 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.yinglan.circleviewlibrary.CircleAlarmTimerView;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "mainactivity";
     private EditText newPhoneNumberText;
-
+    private CircleAlarmTimerView timerView;
+    private String startTime;
+    private String stopTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,13 +37,13 @@ public class MainActivity extends AppCompatActivity {
         ListView phoneNumberListView = (ListView) findViewById(R.id.phoneNumberListView);
         phoneNumberListView.setAdapter(phoneNumberAdapter);
         phoneNumberListView.setOnItemClickListener(
-                new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                        String clickedNumber = String.valueOf(parent.getItemAtPosition(position));
-                        Toast.makeText(MainActivity.this, clickedNumber, Toast.LENGTH_LONG).show();
-                    }
+            new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    String clickedNumber = String.valueOf(parent.getItemAtPosition(position));
+                    Toast.makeText(MainActivity.this, clickedNumber, Toast.LENGTH_LONG).show();
                 }
+            }
         );
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -50,6 +55,36 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Log.d(TAG, "Click");
                 newPhoneNumberText.setVisibility(View.VISIBLE);
+            }
+        });
+        manageTimer();
+        manageStartForwardingButton();
+    }
+
+    private void manageTimer() {
+        final TextView startTimeText = (TextView) findViewById(R.id.startTimeText);
+        final TextView stopTimeText = (TextView) findViewById(R.id.stopTimeText);
+        timerView = (CircleAlarmTimerView) findViewById(R.id.circletimerview);
+        timerView.setOnTimeChangedListener(new CircleAlarmTimerView.OnTimeChangedListener() {
+            @Override
+            public void start(String starting) {
+                MainActivity.this.startTime = starting;
+                startTimeText.setText(starting);
+            }
+
+            @Override
+            public void end(String ending) {
+                MainActivity.this.stopTime = ending;
+                stopTimeText.setText(ending);
+            }
+        });
+    }
+
+    private void manageStartForwardingButton() {
+        final Button startForwardingButton = (Button) findViewById(R.id.startForwardingButton);
+        startForwardingButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.d(TAG, MainActivity.this.startTime);
             }
         });
     }
